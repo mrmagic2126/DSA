1c36abb0f465cfd2017a2f44efd062827b543d57
mrmagic2126 <rakshithytexp@gmail.com>
2026-02-16 21:37:23 +0530

    Update LeetCode solution: linked list cycle (c++)

--- LeetCode/linked-list-cycle.cpp
+++ LeetCode/linked-list-cycle.cpp
@@ -1,8 +1,10 @@
- * };
- */
-class Solution {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        
-    }
-};
