34e020beeeaf034bc8b6bf90c6cfe50e5566725e
mrmagic2126 <rakshithytexp@gmail.com>
2026-02-16 21:25:38 +0530

    Add LeetCode solution: linked list cycle (python)

---
+++ LeetCode/linked-list-cycle.py
@@ -0,0 +1,12 @@
+    bool hasCycle(ListNode *head) {
+        int n =0;
+class Solution {
+public:
+ * };
+ */
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * Definition for singly-linked list.
+ * struct ListNode {
+/**
